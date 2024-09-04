510dedb67a106bf62b0e7775c3512a2ca41e3372
FThappy <thangchu766@gmail.com>
2024-09-05 00:06:10 +0700

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,16 @@
 import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './components/layout/main-layout/main-layout.component';
+import { UserPageComponent } from './components/page/user-page/user-page.component';
 
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent, // Layout chính có header và footer
-    // children: [
-    //   { path: '', component: HomeComponent },
-    //   { path: '', component: AboutComponent }
-    // ]
+    children: [
+      // { path: '', component: HomeComponent },
+      { path: 'users', component: UserPageComponent }
+    ]
   },
 //   {
 //     path: 'auth',
